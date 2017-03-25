90062245de16ae442fbdca095bb98aa929d19bb8
NaoshiHoshi <hoshinaoshi@gmail.com>
2017-03-25 13:37:17 +0900

    headタグをreact化

---
+++ js/containers/Head.js
@@ -0,0 +1,24 @@
+import React, { Component, PropTypes } from 'react';
+import { Helmet } from "react-helmet";
+
+export default class Head extends Component {
+  render() {
+    return (
+      <Helmet>
+        <meta name="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="Content―Language" content="ja" />
+        <meta name="Content―Style-Type" content="text/css" />
+        <meta name="Content―Script-Type" content="text/javascript" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
+
+        <title>従量制のクラウドストレージ - ユカシタ</title>
+        
+        <meta name="keywords" content="従量制, ファイル,クラウド,効率,高速,大容量" />
+        <meta name="description" content="ユカシタは実際に使用した分だけお支払いいただくクラウドストレージサービスです。ライセンスや複雑な契約がなく、シンプルな従量制です。容量も無制限>にご利用いただけます。" />
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap-theme.min.css" />
+      </Helmet>
+    )
+  }
+}
--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as bootstrap from 'react-bootstrap';
+import Head from './containers/Head';
 import Top from './containers/Top';
 
 ReactDOM.render(
   <div>
+    <Head />
     <Top />
   </div>,
   document.getElementById('main')
