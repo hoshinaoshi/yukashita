3e4d64546b2c35abb378f9b8b1f092f7f49a16a4
NaoshiHoshi <hoshinaoshi@gmail.com>
2017-03-26 12:30:44 +0900

    ユーザ認証部分

--- js/containers/Top.js
+++ js/containers/Top.js
@@ -1,14 +1,17 @@
 import React, { Component, PropTypes } from 'react';
 import Header from '../components/Header';
+import SignUp from '../components/SignUp';
 import MainSection from '../components/MainSection';
 import Register from '../components/Register';
 import Footer from '../components/Footer';
 
 export default class Top extends Component {
+
   render() {
     return (
       <div>
         <Header />
+        <SignUp />
         <MainSection />
         <Register />
         <Footer />
---
+++ js/utils/aws-config.js
@@ -0,0 +1,6 @@
+export default {
+  region: 'us-west-2',
+  IdentityPoolId: 'us-west-2:3614712d-5a4a-4fd3-b0da-9ebc9b7be1eb',
+  UserPoolId: 'us-west-2_QbJcF2OZB',
+  ClientId: 'b5hlg8g70b85j3jnr4l702mad',
+}
