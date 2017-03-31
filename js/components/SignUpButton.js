bea2302521a8b7b653defb423d46ed8aa4d0be5b
NaoshiHoshi <hoshinaoshi@gmail.com>
2017-03-31 09:35:35 +0900

    build signup

--- js/components/SignUpButton.js
+++ js/components/SignUpButton.js
@@ -1,10 +1,67 @@
 import React, { Component, PropTypes } from 'react';
-import { Button, Collapse, Modal } from 'react-bootstrap';
+
+// AWS
+import awsConfig from "../utils/aws-config.js";
+import {Config, CognitoIdentityCredentials} from "aws-sdk";
+import { CognitoUserPool, CognitoUserAttribute } from "amazon-cognito-identity-js";
+
+import { Button, Grid, Row, Col, Glyphicon, Image, Collapse, Modal, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+
+Config.region = awsConfig.region;
+Config.credentials = new CognitoIdentityCredentials({
+  IdentityPoolId: awsConfig.IdentityPoolId
+});
+
+const userPool = new CognitoUserPool({
+  UserPoolId: awsConfig.UserPoolId,
+  ClientId:   awsConfig.ClientId,
+});
 
 export default class SignUpButton extends Component {
   constructor(...args) {
     super(...args);
-    this.state = {};
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleEmailChange(e) {
+    this.setState({email: e.target.value});
+  }
+
+  handlePasswordChange(e) {
+    this.setState({password: e.target.value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const email = this.state.email.trim();
+    const password = this.state.password.trim();
+    const attributeList = [
+      new CognitoUserAttribute({
+        Name: 'email',
+        Value: email,
+      })
+    ];
+    userPool.signUp(email, password, attributeList, null, (err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log('user name is ' + result.user.getUsername());
+      console.log('call result: ' + result);
+    });
+  }
+
+  FieldGroup({ id, label, help, ...props }) {
+    return (
+      <FormGroup controlId={id}>
+	<ControlLabel>{label}</ControlLabel>
+	<FormControl {...props} />
+	{help && <HelpBlock>{help}</HelpBlock>}
+      </FormGroup>
+    );
   }
   render() {
     return (
@@ -14,17 +71,34 @@ export default class SignUpButton extends Component {
           <div className="static-modal">
             <Modal.Dialog>
               <Modal.Header>
-                <Modal.Title>Modal title</Modal.Title>
+                <Modal.Title>会員登録</Modal.Title>
               </Modal.Header>
 
-              <Modal.Body>
-                One fine body...
-              </Modal.Body>
+              <form onSubmit={this.handleSubmit.bind(this)}>
+                <Modal.Body>
+                    <this.FieldGroup
+                      id="formControlsEmail"
+                      type="email"
+                      label="Email address"
+                      value={this.state.email}
+                      placeholder="メールアドレス"
+                      onChange={this.handleEmailChange.bind(this)}
+                    />
+                    <this.FieldGroup
+                      id="formControlsPassword"
+                      label="Password"
+                      value={this.state.password}
+                      type="password"
+                      placeholder="パスワード"
+                      onChange={this.handlePasswordChange.bind(this)}
+                    />
+                </Modal.Body>
 
-              <Modal.Footer>
-                <Button>Close</Button>
-                <Button bsStyle="primary">Save changes</Button>
-              </Modal.Footer>
+                <Modal.Footer>
+                  <Button>キャンセル</Button>
+                  <Button type="submit" bsStyle="primary">登録</Button>
+                </Modal.Footer>
+              </form>
 
             </Modal.Dialog>
           </div>
