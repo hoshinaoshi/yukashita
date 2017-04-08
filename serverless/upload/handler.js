2c3b392d8f377ad2e10aa8a385436fddcfc5f14f
NaoshiHoshi <hoshinaoshi@gmail.com>
2017-04-08 16:18:03 +0900

    アップロード完成

--- serverless/upload/handler.js
+++ serverless/upload/handler.js
@@ -3,24 +3,26 @@
 module.exports.upload = (event, context, callback) => {
   var AWS = require('aws-sdk');
   AWS.config.region = 'us-west-2';
-  var bucketName = 'yukashita-image-uploads';
 
   var options = {
     params: {
-      Bucket: bucketName
+      apiVersion: '2006-03-01',
+      Bucket: "yukashita-image-uploads"
     }
   };
+  var bucket = new AWS.S3(options)
 
-  const params = JSON.parse(event.body); 
+  const json = JSON.parse(event.body); 
   var params = {
-    Key: params.fileName,
-    Body: params.image
+    Key: "original-files/" + json.fileName,
+    ContentType: json.type,
+    Body: json.image
   };
-  s3bucket.upload(params, function(err, data) {
+  bucket.putObject(params, function(err, data) {
     if (err) {
       console.log("Error uploading data: ", err);
     } else {
-      console.log("Successfully uploaded data to ");
+      console.log("Successfully uploaded data to " + data);
     }
     context.done(null, 'Finished UploadObjectOnS3');
   });
